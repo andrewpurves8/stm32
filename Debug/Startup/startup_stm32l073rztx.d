928509600ddc622c4d083e1b0695660c6134a0ef
Andrew Purves <andrew.purves@ict.nl>
2024-01-23 15:48:40 +0100

    Removed Debug folder, updated gitignore

--- Debug/Startup/startup_stm32l073rztx.d
+++
@@ -1,1 +0,0 @@
-Startup/startup_stm32l073rztx.o: ../Startup/startup_stm32l073rztx.s
--- Debug/drivers/Src/stm32l07xx_gpio_driver.d
+++
@@ -1,6 +0,0 @@
-drivers/Src/stm32l07xx_gpio_driver.o: \
- ../drivers/Src/stm32l07xx_gpio_driver.c \
- C:/Users/andrew.purves/dev/mcu_course/drivers/drivers/Inc/stm32l07xx_gpio_driver.h \
- C:/Users/andrew.purves/dev/mcu_course/drivers/drivers/Inc/stm32l07xx.h
-C:/Users/andrew.purves/dev/mcu_course/drivers/drivers/Inc/stm32l07xx_gpio_driver.h:
-C:/Users/andrew.purves/dev/mcu_course/drivers/drivers/Inc/stm32l07xx.h:
